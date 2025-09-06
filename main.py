8fb84851e241207bff7d9e3f6438a317b668d6fa
Raffay Rowland <rr01158@surrey.ac.uk>
2025-09-06 19:26:25 +0100

    added requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from define_zones import getSectors, plotSectors, MarshalSectorData, addDataToSectors, plotSectors3D, plotSectors3DByTyre
 import fastf1
 
-session = fastf1.get_session(2025, "Miami", "R")
+session = fastf1.get_session(2024, "Silverstone", "R")
 session.load(laps=True)
 
 sectors = getSectors(session)
